bfc2b2230c4838747e5bc4131a5b5abacde553ed
Craneworks Oy Ltd <craneworks@juego.craneworks.fi>
2015-07-27 10:15:03 +0300

    Improved datetime provider and merged with date provider. The later's class has been removed.

--- crud/providers/DateTimeProvider.php
+++ crud/providers/DateTimeProvider.php
@@ -14,21 +14,25 @@ class DateTimeProvider extends \schmunk42\giiant\base\Provider
 
         $column = $this->generator->getTableSchema()->columns[$attribute];
 
-        if ($column->type == "datetime") {
+        if (stripos($column->type, 'date') !== false) {
             $this->generator->requires[] = 'zhuravljov/yii2-datetime-widgets';
 
-            return <<<EOS
-\$form->field(\$model, '{$column->name}')->widget(\zhuravljov\widgets\DateTimePicker::className(), [
+            return "\$form->field(\$model, '{$column->name}')->widget(\\zhuravljov\\widgets\\DateTimePicker::className(), [
     'options' => ['class' => 'form-control'],
     'clientOptions' => [
         'autoclose' => true,
         'todayHighlight' => true,
-        // Tip: you can switch comments if you only want to display date
-        'format' => 'dd.mm.yyyy hh.ii',
-        // 'format' => 'dd.mm.yyyy',
+        " . ($column->type == "datetime" ?
+                "'format' => 'dd.mm.yyyy hh.ii', // or 'dd.mm.yyyy' to display only the date" :
+                "'format' => 'dd.mm.yyyy',"
+            ) . "
     ],
 ])
-EOS;
+";
+        }
+
+        if ($column->type == "datetime") {
+
         }
     return null;
     }
